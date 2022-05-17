1071219c425d77b2fc66d96b01cf7c2d454810f7
riteshsoni123 <comedybuddy1@gmail.com>
2022-05-17 23:23:10 +0530

    first commit

---
+++ client/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <div>Hi my name is ritesh</div>
+  </React.StrictMode>
+);
